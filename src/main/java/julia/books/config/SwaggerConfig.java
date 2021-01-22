1de845ad9bdbb6170943514f1363a0155ac0f9f2
Iuliia <iuliiaponomareva@gmail.com>
2021-01-22 15:42:25 +0300

    Fix Swagger config

--- src/main/java/julia/books/config/SwaggerConfig.java
+++ src/main/java/julia/books/config/SwaggerConfig.java
@@ -3,11 +3,17 @@ package julia.books.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.security.Principal;
 
 @Configuration
 @EnableSwagger2
@@ -17,8 +23,20 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("julia.books"))
                 .paths(PathSelectors.any())
+                .build()
+                .ignoredParameterTypes(Authentication.class, Principal.class)
+                .tags(
+                        new Tag("Books", "Books operations"),
+                        new Tag("Account management", "Account operations"),
+                        new Tag("Authentication", "Log in and log out"))
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Books Service")
                 .build();
     }
 }
