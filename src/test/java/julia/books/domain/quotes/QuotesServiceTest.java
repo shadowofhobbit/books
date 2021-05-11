5cf81c2ec8bc626467fc1cddb6cf6ea2b0853e4e
Iuliia <iuliiaponomareva@gmail.com>
2021-05-11 18:08:17 +0300

    Add tests

--- src/test/java/julia/books/domain/quotes/QuotesServiceTest.java
+++ src/test/java/julia/books/domain/quotes/QuotesServiceTest.java
@@ -8,11 +8,16 @@ import julia.books.security.UserDetailsServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.client.HttpClientErrorException;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +38,8 @@ class QuotesServiceTest {
     private BookEntity bookEntity;
     private AccountEntity accountEntity;
     private QuoteDTO expectedDto;
+    private QuoteEntity quoteEntity2;
+    private QuoteDTO expectedDto2;
 
     @BeforeEach
     void setUp() {
@@ -50,11 +57,40 @@ class QuotesServiceTest {
         accountEntity.setId(3);
         quoteEntity.setBook(bookEntity);
         quoteEntity.setReader(accountEntity);
+        quoteEntity2 = new QuoteEntity();
+        quoteEntity2.setId(2L);
+        quoteEntity2.setContent("Test2");
+        quoteEntity2.setBook(bookEntity);
+        quoteEntity2.setReader(accountEntity);
         expectedDto = new QuoteDTO();
         expectedDto.setId(1L);
         expectedDto.setBookId(2L);
         expectedDto.setContent("Test");
         expectedDto.setReaderId(3);
+        expectedDto2 = new QuoteDTO();
+        expectedDto2.setId(2L);
+        expectedDto2.setBookId(2L);
+        expectedDto2.setContent("Test");
+        expectedDto2.setReaderId(3);
+    }
+
+    @Test
+    void add() {
+        var dto = new QuoteDTO();
+        dto.setContent("Test");
+        dto.setBookId(2L);
+        dto.setReaderId(3);
+        var entity = new QuoteEntity();
+        entity.setContent("Test");
+        when(mapper.toEntity(dto)).thenReturn(entity);
+        when(booksRepository.getOne(2L)).thenReturn(bookEntity);
+        when(accountRepository.getOne(3)).thenReturn(accountEntity);
+        quotesService.add(dto);
+        verify(mapper).toEntity(dto);
+        verify(booksRepository).getOne(2L);
+        verify(accountRepository).getOne(3);
+        verify(quotesRepository).save(any());
+        verify(mapper).toDto(any());
     }
 
     @Test
@@ -73,6 +109,65 @@ class QuotesServiceTest {
         verify(quotesRepository).findById(1L);
         verify(mapper).toDto(quoteEntity);
         assertEquals(expectedDto, actualDto.orElseThrow());
+    }
+
+    @Test
+    void getQuotesForBook() {
+        when(quotesRepository.findByBookId(anyLong(), any())).thenReturn(
+                new PageImpl<>(List.of(quoteEntity, quoteEntity2), PageRequest.of(0, 10), 2));
+        when(mapper.toDto(quoteEntity)).thenReturn(expectedDto);
+        when(mapper.toDto(quoteEntity2)).thenReturn(expectedDto2);
+        var searchResult = quotesService.getQuotesForBook(2L, 0, 10);
+        verify(quotesRepository).findByBookId(eq(2L), any());
+        assertEquals(List.of(expectedDto, expectedDto2), searchResult.getContent());
+        assertEquals(0, searchResult.getPage());
+        assertEquals(10, searchResult.getSize());
+        assertEquals(2, searchResult.getTotalElements());
+    }
+
+    @Test
+    void getQuotesByUserId() {
+        when(quotesRepository.findByReaderId(anyInt(), any())).thenReturn(
+                new PageImpl<>(List.of(quoteEntity, quoteEntity2), PageRequest.of(0, 10), 2));
+        when(mapper.toDto(quoteEntity)).thenReturn(expectedDto);
+        when(mapper.toDto(quoteEntity2)).thenReturn(expectedDto2);
+        var searchResult = quotesService.getQuotesByUserId(3, 0, 10);
+        verify(quotesRepository).findByReaderId(eq(3), any());
+        assertEquals(List.of(expectedDto, expectedDto2), searchResult.getContent());
+        assertEquals(0, searchResult.getPage());
+        assertEquals(10, searchResult.getSize());
+        assertEquals(2, searchResult.getTotalElements());
+    }
+
+    @Test
+    void update() {
+        var dto = new QuoteDTO();
+        dto.setId(1L);
+        dto.setContent("Updated test");
+        dto.setBookId(2L);
+        dto.setReaderId(3);
+        var updatedDto = new QuoteDTO();
+        updatedDto.setId(1L);
+        updatedDto.setContent("Updated test");
+        updatedDto.setBookId(2L);
+        updatedDto.setReaderId(3);
+        when(quotesRepository.findById(1L)).thenReturn(Optional.of(quoteEntity));
+        when(mapper.toDto(any())).thenReturn(updatedDto);
+        quotesService.update(dto);
+        var captor = ArgumentCaptor.forClass(QuoteEntity.class);
+        verify(quotesRepository).save(captor.capture());
+        assertEquals("Updated test", captor.getValue().getContent());
+    }
+
+    @Test
+    void updateOtherUserQuote() {
+        var dto = new QuoteDTO();
+        dto.setId(1L);
+        dto.setContent("Updated test");
+        dto.setBookId(2L);
+        dto.setReaderId(4);
+        when(quotesRepository.findById(1L)).thenReturn(Optional.of(quoteEntity));
+        assertThrows(AccessDeniedException.class, () -> quotesService.update(dto));
     }
 
     @Test
@@ -103,8 +198,14 @@ class QuotesServiceTest {
                 .roles("USER")
                 .build(), 2);
         when(quotesRepository.findById(1L)).thenReturn(Optional.of(quoteEntity));
-        assertThrows(HttpClientErrorException.class, () -> quotesService.delete(1L, customUser));
+        assertThrows(AccessDeniedException.class, () -> quotesService.delete(1L, customUser));
         verifyNoMoreInteractions(quotesRepository);
+    }
+
+    @Test
+    void publishNonExistingQuote() {
+        when(quotesRepository.findById(1L)).thenReturn(Optional.empty());
+        assertThrows(NoSuchElementException.class, () -> quotesService.publishInTelegramBot(1L));
     }
 
 }
